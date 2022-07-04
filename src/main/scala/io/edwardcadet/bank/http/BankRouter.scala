a65931de47994845115cc27f977281847c402b45
TedCadet <e_cadet22@hotmail.com>
2022-07-03 21:15:35 -0400

    modif de "BankRouter" pour ajouter la validation des requetes

--- src/main/scala/io/edwardcadet/bank/http/BankRouter.scala
+++ src/main/scala/io/edwardcadet/bank/http/BankRouter.scala
@@ -6,15 +6,21 @@ import akka.http.scaladsl.model.StatusCodes
 import akka.http.scaladsl.model.headers.Location
 import akka.http.scaladsl.server
 import akka.http.scaladsl.server.Directives._
+import akka.http.scaladsl.server.Route
 import akka.util.Timeout
+import cats.data.Validated.{Invalid, Valid}
 import io.edwardcadet.bank.actors.PersistentBankAccount.{BankAccount, Command, Response}
 import io.circe.generic.auto._
 import de.heikoseeberger.akkahttpcirce.FailFastCirceSupport._
 import io.edwardcadet.bank.actors.PersistentBankAccount.Commands.{CreateBankAccount, GetBankAccount, UpdateBalance}
 import io.edwardcadet.bank.actors.PersistentBankAccount.Responses.{BankAccountBalanceUpdatedResponse, BankAccountCreatedResponse, GetBankAccountResponse}
+import io.edwardcadet.bank.http.Validation.{ValidationResult, Validator, required, validateEntity, validateMinimum, validateMinimumAbs, validateRequired}
 
 import scala.concurrent.Future
 import scala.concurrent.duration.DurationInt
+import cats.implicits._
+
+import scala.util.{Failure, Success}
 
 class BankRouter(bank: ActorRef[Command])(implicit system: ActorSystem[_]) {
   /*
@@ -50,17 +56,20 @@ class BankRouter(bank: ActorRef[Command])(implicit system: ActorSystem[_]) {
       post {
         // parse the payload
         entity(as[BankAccountCreationRequest]) { request =>
-          /*
+          // validation
+          validateRequest(request) {
+            /*
           * convert the request into a Command for the bank actor
           * send the command to the bank
           * expect a reply
           * send back an HTTP response
            */
-          onSuccess(createBankAccount(request)) {
-            case BankAccountCreatedResponse(id) =>
-              respondWithHeader(Location(s"/bank/$id")) {
-                complete(StatusCodes.Created)
-              }
+            onSuccess(createBankAccount(request)) {
+              case BankAccountCreatedResponse(id) =>
+                respondWithHeader(Location(s"/bank/$id")) {
+                  complete(StatusCodes.Created)
+                }
+            }
           }
         }
       }
@@ -86,13 +95,15 @@ class BankRouter(bank: ActorRef[Command])(implicit system: ActorSystem[_]) {
            * expect a reply
            * send back an HTTPResponse
             */
-            // TODO validate the request
+            // validate the request
             entity(as[BankAccountUpdateRequest]) { request =>
-              onSuccess(updateBankAccount(id, request)) {
-                case BankAccountBalanceUpdatedResponse(Some(account)) =>
-                  complete(account)
-                case BankAccountBalanceUpdatedResponse(None) =>
-                  complete(StatusCodes.NotFound, FailureResponse(s"bank account $id not found"))
+              validateRequest(request) {
+                onSuccess(updateBankAccount(id, request)) {
+                  case BankAccountBalanceUpdatedResponse(Success(account)) =>
+                    complete(account)
+                  case BankAccountBalanceUpdatedResponse(Failure(ex)) =>
+                    complete(StatusCodes.BadRequest, FailureResponse(s"${ex.getMessage}"))
+                }
               }
             }
           }
@@ -109,9 +120,19 @@ class BankRouter(bank: ActorRef[Command])(implicit system: ActorSystem[_]) {
 
   def updateBankAccount(id: String, request: BankAccountUpdateRequest): Future[Response] =
     bank.ask(replyTo => request.toCommand(id, replyTo))
+
+  def validateRequest[R: Validator](request: R)(routeIfValid: Route): Route =
+    validateEntity(request) match {
+      case Valid(_) =>
+        routeIfValid
+      case Invalid(failures) =>
+        complete(StatusCodes.BadRequest,
+          FailureResponse(failures.toList.map(_.errorMessage).mkString(", ")))
+
+    }
 }
 
-// used to receive a request from a consumer
+// DAO
 // toCommand will translate the request to a command
 
 case class BankAccountCreationRequest(user: String, currency: String, balance: Double) {
@@ -121,11 +142,31 @@ case class BankAccountCreationRequest(user: String, currency: String, balance: D
     replyTo)
 }
 
+object BankAccountCreationRequest {
+  implicit val validator: Validator[BankAccountCreationRequest] = (request: BankAccountCreationRequest) => {
+    val userValidation = validateRequired("user", request.user)
+    val currencyValidation = validateRequired("currency", request.currency)
+    val balanceValidation = validateMinimum("balance", request.balance, 0)
+      .combine(validateMinimumAbs("balance", request.balance, 0))
+
+    (userValidation, currencyValidation, balanceValidation).mapN(BankAccountCreationRequest.apply)
+  }
+}
+
 case class BankAccountUpdateRequest(currency: String, amount: Double) {
   def toCommand(id: String, replyTo: ActorRef[Response]): Command = UpdateBalance(id,
     currency,
     amount,
     replyTo)
+}
+
+object BankAccountUpdateRequest {
+  implicit val validator: Validator[BankAccountUpdateRequest] = (request: BankAccountUpdateRequest) => {
+    val currencyValidation = validateRequired("currency", request.currency)
+    val amountValidation = validateMinimumAbs("amount", request.amount, 0.01)
+
+    (currencyValidation, amountValidation).mapN(BankAccountUpdateRequest.apply)
+  }
 }
 
 case class FailureResponse(reason: String)
